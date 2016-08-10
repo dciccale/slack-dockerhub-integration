4f2889fbad447ce1a0f50f5aa331e0f2062bac77
Denis Ciccale <dciccale@gmail.com>
2016-08-10 11:27:33 +0200

    es6ify

--- index.js
+++ index.js
@@ -2,53 +2,46 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var fs = require('fs');
-var qs = require('querystring');
-var Hapi = require('hapi');
-var inert = require('inert');
-var marked = require('marked');
+const fs = require('fs');
+const Hapi = require('hapi');
+const inert = require('inert');
+const marked = require('marked');
 
-var SlackService = require('./slack.service.js');
+const SlackService = require('./slack.service.js');
 
-var server = new Hapi.Server();
-var port = process.env.PORT || 8080;
+const server = new Hapi.Server();
+const port = process.env.PORT || 8080;
 
 server.connection({port: port});
-server.register(inert, function () {});
+server.register(inert, () => {});
 
-var readme = fs.readFileSync(__dirname + '/README.md', 'utf8');
-var docs = marked(readme);
+const readme = fs.readFileSync(`${__dirname}/README.md`, 'utf8');
+const docs = marked(readme);
 
 server.route({
   method: 'GET',
   path:'/{param*}',
-  handler: function (req, res) {
-    res(docs);
-  }
+  handler: (req, res) => res(docs))
 });
 
 server.route({
   method: 'GET',
   path: '/docker-logo.png',
-  handler: function (req, res) {
-    res.file(__dirname + '/docker-logo.png');
-  }
+  handler: (req, res) => res.file(`${__dirname}/docker-logo.png`))
 });
 
 server.route({
   method: 'POST',
   path:'/services/{id*}',
   handler: function (req, res) {
-    var slackService = new SlackService(req.params);
+    const slackService = new SlackService(req.params);
 
     slackService.send({
-      text: '[<' + req.payload.repository.repo_url + '|' + req.payload.repository.repo_name + '>] new image build complete',
+      text: `[<${req.payload.repository.repo_url}|${req.payload.repository.repo_name}>] new image build complete`,
       username: 'DockerHub',
       icon_url: 'https://hooks-slack-dockerhub.herokuapp.com/docker-logo.png'
     }, res);
   }
 });
 
-server.start(function () {
-  console.log('Server listening on %d', port);
-});
+server.start(() => console.log('Server listening on %d', port));
