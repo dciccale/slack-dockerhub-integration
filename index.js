f767090c684cdb369d6a70e645d2e178681f308a
Denis Ciccale <dciccale@gmail.com>
2016-08-10 11:43:43 +0200

    es6ify and put everything in 1 file

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+const https = require('https');
 const fs = require('fs');
 const Hapi = require('hapi');
 const inert = require('inert');
 const marked = require('marked');
-
-const SlackService = require('./slack.service.js');
 
 const server = new Hapi.Server();
 const port = process.env.PORT || 8080;
@@ -20,27 +19,47 @@ const docs = marked(readme);
 
 server.route({
   method: 'GET',
-  path:'/{param*}',
-  handler: (req, res) => res(docs)
+  path: '/{param*}',
+  handler: (req, reply) => reply(docs)
 });
 
 server.route({
   method: 'GET',
   path: '/docker-logo.png',
-  handler: (req, res) => res.file(`${__dirname}/docker-logo.png`)
+  handler: (req, reply) => reply.file(`${__dirname}/docker-logo.png`)
 });
 
 server.route({
   method: 'POST',
-  path:'/services/{id*}',
-  handler: function (req, res) {
-    const slackService = new SlackService(req.params);
-
-    slackService.send({
+  path: '/services/{id*}',
+  handler: (req, reply) => {
+    const params = {
       text: `[<${req.payload.repository.repo_url}|${req.payload.repository.repo_name}>] new image build complete`,
       username: 'DockerHub',
       icon_url: 'https://hooks-slack-dockerhub.herokuapp.com/docker-logo.png'
-    }, res);
+    };
+
+    const options = {
+      method: 'POST',
+      hostname: 'hooks.slack.com',
+      path: `/services/${req.params.id}`,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    let body = '';
+
+    https.request(options, (res) => {
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', () => reply(body));
+    })
+    .on('error', reply)
+    .write(JSON.stringify(params))
+    .end();
   }
 });
 
